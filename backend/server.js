d70c6e5d86582e5b098b055fecea21079244afa2
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-08-10 20:01:44 +0300

    chore: Fix formatting

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 const colors = require('colors');
 const dotenv = require('dotenv');
-const errorHandler = require('./middlware/errorMiddleware.js')
-dotenv.config()
-const PORT = process.env.PORT || 7000;
+const errorHandler = require('./middlware/errorMiddleware.js');
+dotenv.config();
+const PORT = process.env.PORT || 7007;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/goals', require('./routers/goalRouter'))
-app.use('/api/v1/users', require('./routers/userRouter'))
+app.use('/api/v1/goals', require('./routers/goalRouter'));
+app.use('/api/v1/users', require('./routers/userRouter'));
 
 app.use(errorHandler); // need to be after Router use
 
-app.listen(PORT, () => console.log(`HI! You may come in here: http://localhost:${PORT}`.magenta))
+app.listen(PORT, () => console.log(`HI! You may come in here: http://localhost:${PORT}`.magenta));
 connectDB();
-
